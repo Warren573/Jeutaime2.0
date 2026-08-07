33d6cfb3836b3af9db0a3cecea20ebf4fdc22d36
Warren573 <warren.andrz@gmail.com>
2026-08-07 18:44:17 +0200

    Assemble illustrated refuge background data

---
+++ frontend/src/assets/refugeBackgroundData.ts
@@ -0,0 +1,5 @@
+import part0 from './refugeBackgroundChunks/tiny0';
+import part1 from './refugeBackgroundChunks/tiny1';
+import part2 from './refugeBackgroundChunks/tiny2';
+
+export const REFUGE_BACKGROUND_DATA_URI = `data:image/jpeg;base64,${part0}${part1}${part2}`;
