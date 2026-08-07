0bbe0bebd86d518be98929052b05ad5691902a7d
Warren573 <warren.andrz@gmail.com>
2026-08-07 15:54:24 +0200

    feat: wire password settings screen

--- frontend/app/password.tsx
+++ frontend/app/password.tsx
@@ -1,4 +1,3 @@
-import PlaceholderScreen from '../src/screens/PlaceholderScreen';
-export default function Screen() {
-  return <PlaceholderScreen icon="🔑" title="Mot de passe et connexion" description="Modifiez votre mot de passe et méthodes de connexion." />;
-}
+import PasswordSettingsScreen from '../src/screens/PasswordSettingsScreen';
+
+export default PasswordSettingsScreen;
